532c3165a0e2b94031bb1eba5abc13539e1e3423
akankshajavvaji <136601701+akankshajavvaji@users.noreply.github.com>
2025-02-04 09:33:53 +0530

    Add files via upload

---
+++ main-2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* This code provides the sum 0f 2 integers*/
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    int sum = a+b;
+    cout << sum << endl;
+}
