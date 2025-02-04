df4bdd0e6c6fbf44f54edea1cf0a801d3393e06a
akankshajavvaji <136601701+akankshajavvaji@users.noreply.github.com>
2025-02-04 09:40:48 +0530

    Add files via upload

---
+++ main-4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* This code provides the sum 0f 2 integers*/
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    int sum = a+b;
+    int sub = a-b;
+    int mul = a*b;
+    int divv = a/b;
+    cout << sum << endl;
+    cout << sub << endl;
+    cout << mul << endl;
+    cout << divv << endl;
+}
